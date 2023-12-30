b50cebae1f0a385a5a57d933e4ebd764bf560fee
adibmahmud007 <adibmahmud307@gmail.com>
2023-12-30 15:47:28 +0600

    Toast modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 import Courses from './components/Courses/Courses'
 import Credits from './components/Credits/Credits'
 import Header from './components/Header/Header'
-import { toast } from 'react-toastify';
+import { Toaster } from 'react-hot-toast';
+import {toast} from 'react-hot-toast';
+
 import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -17,32 +19,50 @@ function App() {
 
 
 const handlePrice = (cprice, ctitle, credit_hour) => {
-  if (!title.includes(ctitle)) {
-    const newTotalCredit = credit + credit_hour;
+    if (!title.includes(ctitle)) {
+      const newTotalCredit = credit + credit_hour;
 
-    // the minimum number should not be less than 0
-    const limitedTotalCredit = Math.max(newTotalCredit, 0);
+      // The minimum number should not be less than 0
+      const limitedTotalCredit = Math.max(newTotalCredit, 0);
 
-    // Only update state if the credits are within the limit
-    if (limitedTotalCredit <= 20) {
-      setTitle(newTitles => [...newTitles, ctitle]);
-      setPrice(newPrice => newPrice + cprice);
-      setCredit(limitedTotalCredit);
+      // Only update state if the credits are within the limit
+      if (limitedTotalCredit <= 20) {
+        setTitle((newTitles) => [...newTitles, ctitle]);
+        setPrice((newPrice) => newPrice + cprice);
+        setCredit(limitedTotalCredit);
 
-      
-      toast.success(`Course "${ctitle}" added successfully!`);
+        
+        toast.success(`Course "${ctitle}" added successfully!`, {
+          style: {
+            background: 'black',
+            color: 'white',
+          },
+        });
+      } else {
+        toast.error(`Credits limit reached. You can not select more courses`,{
+          style:{
+            background: 'black',
+            color: 'red',
+          }
+        });
+      }
     } else {
-      toast.error(`Credits limit reached.`);
+      toast.error(`Course "${ctitle}" is already selected.`,{
+        icon: '',
+        style:{
+          background: 'black',
+          color: 'yellow'
+        }
+      });
+      console.log(`Course "${ctitle}" is already selected.`);
     }
-  } else {
-    toast.warn(`Course "${ctitle}" is already selected.`);
-  }
-};
+  };
 
 
   return (
     <div className='bg-slate-200 '>
       <Header></Header>
+      <Toaster></Toaster>
       <div className='lg:flex lg:flex-row-reverse mb-4 lg:mt-7 '>
         <div className='lg:w-1/4  mb-5 p-4'>
         <Credits className='' price={price} credit={credit} title={title}></Credits>
